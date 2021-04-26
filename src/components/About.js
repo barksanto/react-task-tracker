50185e1dd1bf38e925575b95a884b71782c1da4f
Barkley Santo <barkley.santo@gmail.com>
2021-04-26 14:09:12 -0500

    import react-router-dom replace anchor tags with Link and href with "to"

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom'
+
 const About = () => {
   return (
     <div>
       <h4>Version 1.0.0</h4>
-      <a href="/">Go Back</a>
+      {/* instead of anchor tag - we import link from react router dom
+        instead of href, we use to
+      */}
+      <Link to="/">Go Back</Link>
     </div>
   )
 }
