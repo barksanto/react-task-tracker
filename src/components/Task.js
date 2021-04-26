ddaa0c2dbc76b5fced55340f530d09ada82f324a
Barkley Santo <barkley.santo@gmail.com>
2021-04-26 11:52:49 -0500

    pass delete function through props to Task component through props

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,14 @@
-const Task = () => {
+import { FaTimes } from 'react-icons/fa'
+
+const Task = ({ task, onDelete }) => {
   return (
     <div className="task">
-      <h3>My Task</h3>
+      <h3>{task.text}
+        <FaTimes
+          onClick={onDelete}
+          style={{ color: 'red', cursor: 'pointer' }} />
+      </h3>
+      <p>{task.day}</p>
     </div>
   )
 }
