0cf78ef1033c9d40b8cec42140b3ae606e088d65
Barkley Santo <barkley.santo@gmail.com>
2021-04-26 10:54:57 -0500

    create headingStyle const for h1 style

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+
+
+const Header = ({ title }) => {
+  return (
+    <div>
+      <header>
+        <h1 >{title}</h1>
+      </header>
+    </div>
+  )
+}
+
+// If I wanted to style using an object
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black'
+// }
+
+Header.defaultProps = {
+  title: "Task Tracker",
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default Header
